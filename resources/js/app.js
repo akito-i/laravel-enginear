c5d3e7451269a8ad7c364a863ed1ef7d120dbca4
akito-i <a.imamura0827@gmail.com>
2020-10-17 23:08:24 +0900

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,13 @@
 import './bootstrap'
 import Vue from 'vue'
-import ArticleLike from './components/ArticleLike'
+import Articlelike from './components/Articlelike'
 import ArticleTagsInput from './components/ArticleTagsInput'
 import FollowButton from './components/FollowButton'
 
 const app = new Vue({
   el: '#app',
   components: {
-    ArticleLike,
+    Articlelike,
     ArticleTagsInput,
     FollowButton,
   }
