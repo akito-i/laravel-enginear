9993b833ef8908a3128f2c49ff693bca04eb91ca
akito-i <a.imamura0827@gmail.com>
2020-10-17 22:51:38 +0900

    first-commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,8 @@
 import './bootstrap'
 import Vue from 'vue'
-import ArticleLike from './components/ArticleLike'
-import ArticleTagsInput from './components/ArticleTagsInput'
-import FollowButton from './components/FollowButton'
+import ArticleLike from '@/components/ArticleLike'
+import ArticleTagsInput from '@/components/ArticleTagsInput'
+import FollowButton from '@/components/FollowButton'
 
 const app = new Vue({
   el: '#app',
